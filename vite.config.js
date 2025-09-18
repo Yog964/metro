104b84006ee99def3bb9acbea98ed3120e99368e
Yog964 <bariy964@gmail.com>
2025-09-19 02:46:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    target: 'esnext',
-    outDir: 'dist', // 👈 must stay 'dist' for gh-pages
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-  base: '/metro/', // 👈 required for GitHub Pages (repo name)
+  base: '/metro/',   // 👈 must match your repo name
 })
